32e62aa4d1abacb4fe3561c0e909072b3ae094bc
Saikumartheppa <saikumartheppa06@gmail.com>
2025-08-27 12:56:43 +0530

    Added Title page icon

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import FAVICON from "./assets/youtube_favicon.png";
+const link = document.createElement('link');
+link.rel = 'icon';
+document.head.appendChild(link);
+link.href = FAVICON;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
