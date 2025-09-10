ff0c40028637d387f2964c045acdd5390b2c7294
Saikumartheppa <saikumartheppa06@gmail.com>
2025-09-10 09:46:10 +0530

    Updated videoContainer implemented infinite scroll by utilizing the useCallback and useRef hooks & IntersectionObserver

--- src/components/home/body/main-container/video-container/VideoContainer.jsx
+++ src/components/home/body/main-container/video-container/VideoContainer.jsx
@@ -1,24 +1,74 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import styles from "./style.module.css";
 import { fetchVideos } from "./videoService";
 import { VideoCard } from "./video-card";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 const VideoContainer = () => {
-  const [videos, setVideos] = useState([]);
-  const isMenuOpen = useSelector(store => store.app.isMenuOpen);
+  const dispatch = useDispatch();
+  const { items, nextPageToken, hasMore, status, error } = useSelector(
+    (store) => store.videos
+  );
+  const observer = useRef();
+  const lastVideoRef = useCallback(
+    (node) => {
+      if (status === "loading" || !hasMore) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          console.log("loading next page with token : ", nextPageToken);
+          dispatch(fetchVideos(nextPageToken));
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [status, hasMore, nextPageToken]
+  );
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
   useEffect(() => {
-    getVideos();
-  }, []);
-  const getVideos = async () => {
-    const videoResults = await fetchVideos();
-    setVideos(videoResults);
-  };
+    if (items.length === 0 && status === "idle") {
+      console.log("Initial fetch for videos...");
+      dispatch(fetchVideos());
+    }
+  }, [items.length, status]);
+
   return (
-    <div className={`${styles.videoContainer} ${isMenuOpen ? styles.menuOpen : styles.menuClose}`}>
-      {videos.map((video) => (
-       <Link to={`/watch?v=${video.id}`} key={video.id}> <VideoCard isMenuOpen={isMenuOpen} info={video} /></Link>
-      ))}
+    <div
+      className={`${styles.videoContainer} ${
+        isMenuOpen ? styles.menuOpen : styles.menuClose
+      }`}
+    >
+      {items.map((video, index) => {
+        return index === items.length - 1 ? (
+          <Link to={`/watch?v=${video.id}`} ref={lastVideoRef} key={video.id}>
+            <VideoCard isMenuOpen={isMenuOpen} info={video} />
+          </Link>
+        ) : (
+          <Link to={`/watch?v=${video.id}`} key={video.id}>
+            <VideoCard isMenuOpen={isMenuOpen} info={video} />
+          </Link>
+        );
+      })}
+      {status === "loading" && (
+        <div className="loader">
+            <p>Loading...</p> 
+        </div>
+      )}
+      {status === "failed" && error && (
+        <div className="failed">
+          <p>Error loading videos: {error}</p>
+        </div>
+      )}
+      {!hasMore && items.length > 0 && status !== "loading" && (
+        <div className="reachedEnd">
+          <p>You've reached the end of the videos!</p>
+        </div>
+      )}
+      {items.length === 0 && status === "succeeded" && !hasMore && (
+        <div className="NoVideosFound">
+          <p>No videos found.</p>
+        </div>
+      )}
     </div>
   );
 };
